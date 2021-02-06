e971623ffb5bc30290c5ca66f1bbf1633ec65565
kai <2857693944@qq.com>
2021-02-06 23:07:47 +0800

    Update the source code
    Update the source code

---
+++ README.md
@@ -0,0 +1,27 @@
+[[Chinese]](README_cn.md)
+
+The Project is aim to learn how to use the iot module by Tuya. The Project is to read the temperature and the humidity from the sht30, and show it on the OLED screen and upload it to the tuya cloud. 
+# Introduce
+* get the temperature and the humidity information from the SHT30 sensor on board
+* show the information on the 0.96" OLED screen
+* upload the information to the tuya cloud that I can see the temperature and the humidity in tuya APP.
+
+# Video
+TODO
+# Hardware
+WBR3D WiFi module
+
+SHT30 temperature and humidity sensor
+
+SSD1306 0.96"OLED screen
+
+STM32G431CBT6 microcontroller
+
+[PCB Link](https://oshwhub.com/baobaoa/wu-lian-wang-qi-xiang-tai-657332a)
+# Software
+MCU SDK by Tuya
+
+STM32 CubeMX with HAL
+
+Source Code is in the "source" folder
+
---
+++ software/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/DependInfo.cmake
@@ -0,0 +1,87 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/startup/startup_stm32g431xx.s" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/startup/startup_stm32g431xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32G431xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32G4xx_HAL_Driver/Inc"
+  "../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32G4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/wifi_sdk"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/gpio.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/gpio.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/i2c.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/i2c.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/main.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/main.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/oled.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/oled.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/sht30.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/sht30.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/spi.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/spi.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/stm32g4xx_hal_msp.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/stm32g4xx_hal_msp.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/stm32g4xx_it.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/stm32g4xx_it.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/syscalls.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/syscalls.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/system_stm32g4xx.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/system_stm32g4xx.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Core/Src/usart.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Core/Src/usart.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_i2c.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_i2c.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_i2c_ex.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_i2c_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_spi.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_spi.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_spi_ex.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_spi_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/wifi_sdk/mcu_api.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/wifi_sdk/mcu_api.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/wifi_sdk/protocol.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/wifi_sdk/protocol.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/Drivers/wifi_sdk/system.c" "/Users/kai/Desktop/mySync/mySTProject/tuyaiot/cmake-build-debug/CMakeFiles/tuyaiot.elf.dir/Drivers/wifi_sdk/system.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32G431xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32G4xx_HAL_Driver/Inc"
+  "../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32G4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/wifi_sdk"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
